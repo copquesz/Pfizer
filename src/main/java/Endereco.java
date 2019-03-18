f1ca2a72b948049d23df2c36e4f7712683bd1196
Lucas Copque <copque.faculdade@gmail.com>
2019-03-18 14:00:06 -0300

    model de endereço

---
+++ src/main/java/Endereco.java
@@ -0,0 +1,47 @@
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@Entity
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 348994887454466099L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "logradouro")
+	private String logradouro;
+
+	@Column(name = "numero")
+	private String numero;
+
+	@Column(name = "complemento")
+	private String complemento;
+
+	@Column(name = "bairro")
+	private String bairro;
+
+	@Column(name = "cidade")
+	private String cidade;
+
+	@Column(name = "estado")
+	private String estado;
+
+	@Column(name = "cep")
+	private String cep;
+
+}
