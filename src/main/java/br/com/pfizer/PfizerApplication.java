ca6216209e43288afda0d76010b5914b6b931721
Lucas Copque <copque.faculdade@gmail.com>
2019-03-18 12:32:40 -0300

    starter projeto.

---
+++ src/main/java/br/com/pfizer/PfizerApplication.java
@@ -0,0 +1,13 @@
+package br.com.pfizer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfizerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfizerApplication.class, args);
+	}
+
+}
