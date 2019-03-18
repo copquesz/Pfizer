d63601f61a8342ba37941e924c581fc37f65f26e
Lucas Copque <copque.faculdade@gmail.com>
2019-03-18 15:28:04 -0300

    model de usuario

---
+++ src/main/java/br/com/pfizer/model/Usuario.java
@@ -0,0 +1,64 @@
+package br.com.pfizer.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.pfizer.enumerated.TipoUsuario;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@Entity
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = 7243225367592107901L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date ultimoAcesso;
+	
+	@Temporal(TemporalType.DATE)
+	private Date dataNascimento;
+
+	private String email;
+
+	private String senha;
+
+	private String telefone;
+	
+	private String cpf;
+	
+	private String formacao;
+	
+	@Enumerated(EnumType.ORDINAL)
+	TipoUsuario tipo;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "endereco_id")
+	Endereco endereco;
+
+}
