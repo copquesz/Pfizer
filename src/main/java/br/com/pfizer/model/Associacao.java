59f614ca9333495a443d72544b9ab729535c10e6
Lucas Copque <copque.faculdade@gmail.com>
2019-04-02 14:17:51 -0300

    model de associacao

---
+++ src/main/java/br/com/pfizer/model/Associacao.java
@@ -0,0 +1,72 @@
+package br.com.pfizer.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import br.com.pfizer.enumerated.AreaAtuacao;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+public class Associacao implements Serializable{
+
+	private static final long serialVersionUID = 5120433924069225125L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private Long id;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date dataCadastro;
+	
+	private String razaoSocial;
+	
+	private String nomeFantasia;
+	
+	private String cnpj;
+	
+	private String dataFundacao;
+	
+	private String dataMandato;
+	
+	private Integer qtdFuncionarios;
+	
+	private Integer qtdVoluntarios;
+	
+	@Enumerated(EnumType.ORDINAL)
+	private AreaAtuacao area;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "redes_sociais_id")
+	RedesSociais redesSociais;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "documentos_associacao_id")
+	DocumentosAssociacao documentosAssociacao;
+	
+	@OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	@JoinColumn(name = "endereco_id")
+	Endereco endereco;
+	
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "usuario_id")
+	Usuario usuario;
+}
