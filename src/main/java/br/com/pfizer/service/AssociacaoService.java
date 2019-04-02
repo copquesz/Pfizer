5607f6d92f0bf4bbc1a74555c40cb12be4a8032d
Lucas Copque <copque.faculdade@gmail.com>
2019-04-02 17:15:07 -0300

    adicionando o método que faz upload de arquivos.

--- src/main/java/br/com/pfizer/service/AssociacaoService.java
+++ src/main/java/br/com/pfizer/service/AssociacaoService.java
@@ -3,26 +3,35 @@ package br.com.pfizer.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.pfizer.model.Associacao;
 import br.com.pfizer.repository.AssociacaoRepository;
+import br.com.pfizer.util.FileUpload;
 
 @Service
 public class AssociacaoService {
-	
+
 	private AssociacaoRepository repository;
-	
+
 	public Associacao save(Associacao associacao) {
 		return repository.save(associacao);
 	}
-	
+
 	public Optional<Associacao> findById(Long id) {
 		return repository.findById(id);
 	}
-	
-	public List<Associacao> findAll(){
+
+	public List<Associacao> findAll() {
 		return repository.findAll();
+	}
+
+	// Serviço que faz o upload de arquivos para o servidor
+	public String upload(HttpServletRequest request, MultipartFile arquivo, String nomeArquivo, String url) {
+		return FileUpload.upload(request, arquivo, nomeArquivo, url);
 	}
 
 }
--- src/main/java/br/com/pfizer/service/ProjetoService.java
+++ src/main/java/br/com/pfizer/service/ProjetoService.java
@@ -3,26 +3,35 @@ package br.com.pfizer.service;
 import java.util.List;
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.pfizer.model.Projeto;
 import br.com.pfizer.repository.ProjetoRepository;
+import br.com.pfizer.util.FileUpload;
 
 @Service
 public class ProjetoService {
-	
+
 	private ProjetoRepository repository;
-	
+
 	public Projeto save(Projeto projeto) {
 		return repository.save(projeto);
 	}
-	
+
 	public Optional<Projeto> findById(Long id) {
 		return repository.findById(id);
 	}
-	
-	public List<Projeto> findAll(){
+
+	public List<Projeto> findAll() {
 		return repository.findAll();
+	}
+
+	// Serviço que faz o upload de arquivos para o servidor
+	public String upload(HttpServletRequest request, MultipartFile arquivo, String nomeArquivo, String url) {
+		return FileUpload.upload(request, arquivo, nomeArquivo, url);
 	}
 
 }
