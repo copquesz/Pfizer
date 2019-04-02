4e72a6a365232ef3d8c8d522002494c1dac492fb
Lucas Copque <copque.faculdade@gmail.com>
2019-04-02 13:40:50 -0300

    corrigindo anotações

--- src/main/java/br/com/pfizer/model/Endereco.java
+++ src/main/java/br/com/pfizer/model/Endereco.java
@@ -20,29 +20,21 @@ public class Endereco implements Serializable {
 	private static final long serialVersionUID = 348994887454466099L;
 
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
-
-	@Column(name = "logradouro")
+	
 	private String logradouro;
-
-	@Column(name = "numero")
+	
 	private String numero;
-
-	@Column(name = "complemento")
+	
 	private String complemento;
-
-	@Column(name = "bairro")
+	
 	private String bairro;
-
-	@Column(name = "cidade")
+	
 	private String cidade;
-
-	@Column(name = "estado")
+	
 	private String estado;
-
-	@Column(name = "cep")
+	
 	private String cep;
 
 }
